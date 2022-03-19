f1b0cc8c9f824c8c2ad0ab9b987fe17bc1417479
SammyJenas <94944827+SammyJenas@users.noreply.github.com>
2022-03-19 23:27:41 +1300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+// Sammy Jenas
+// Landing page for GitHub
+// https://sammyjenas.github.io/
+// March 2022
+
+let ip;
+let sammyjenas = 'Sammy Jenas';
+let font1;
+
+function getIP() {
+  fetch('https://api.ipify.org?format=json')
+    .then(results => results.json())
+    .then(data => (ip = data.ip))
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  font1 = loadFont('AcPlus_IBM_VGA_8x16.ttf');
+  textFont(font1);
+  getIP();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+  if (frameCount % 60 === 0) {
+    nameChange();
+  }
+  fill(255);
+  textSize(20);
+  text('You: ' + ip, 70, 70);
+  text('Me: ' + sammyjenas, 70, 110);
+}
+
+function nameChange() {
+  let shift = int(random(0, 5));
+  switch (shift) {
+    case 0:
+      sammyjenas = 'Sammy Jenas';
+      break;
+    case 1:
+      sammyjenas = 'Sammee Jenas';
+      break;
+    case 2:
+      sammyjenas = 'Sami Jenas';
+      break;
+    case 3:
+      sammyjenas = 'Sammy Genus';
+      break;
+    case 4:
+      sammyjenas = 'Sammee Genus';
+      break;
+    case 5:
+      sammyjenas = 'Sami Genus';
+  }
+}
